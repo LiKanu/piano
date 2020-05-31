b2de87adfaa9de3d6afaf6f12393530932bbd2ec
likanug <1452930742@qq.com>
2020-05-31 10:52:39 +0800

    processing 模板代码

---
+++ README.md
@@ -0,0 +1,1 @@
+在VSCode 下开发的processing项目
--- src/main/java/piano/App.java
+++ src/main/java/piano/App.java
@@ -3,12 +3,31 @@
  */
 package piano;
 
-public class App {
-    public String getGreeting() {
-        return "Hello world.";
+import processing.core.PApplet;
+
+public class App extends PApplet{
+
+    public App(){
+
     }
 
+    @Override
+    public void settings() {
+      
+    }
+
+    @Override
+    public void setup() {
+      
+    }
+    
+    @Override
+    public void draw() {
+      
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        PApplet.main("piano.App");
     }
 }
